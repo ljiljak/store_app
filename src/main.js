5acadc59250f747574eb0fd6ca38994f360ae83c
ljiljak <ljiljakaleksandar@gmail.com>
2018-04-27 20:53:57 +0200

    fist ommit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import Customers from './pages/app.customers.vue'
+import Products from './pages/app.products.vue'
+
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter)
+
+const routes = [
+	{ path: '/', redirect: '/customers'},
+	{ path: '/app.customers', component: Customers},
+	{ path: '/app.products', component: Products}
+]
+
+const router = new VueRouter({
+  routes
+})
+
 new Vue({
+	router,
   render: h => h(App)
 }).$mount('#app')
